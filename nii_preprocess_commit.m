c5c1ec7e8461903f948abbabfa6361905021b28c
neurolabusc <rorden@mailbox.sc.edu>
2016-01-20 12:23:23 -0500

    comcbi-mcbi18-Wed Jan 20 12:23:23 EST 2016

--- nii_preprocess_commit.m
+++ nii_preprocess_commit.m
@@ -1,6 +1,6 @@
 function nii_preprocess_commit
-pth = '/usr/local/nii_preprocess/';
-system(sprintf('%snii_preprocess_commit.bat',pth),'-echo');
+%pth = '/usr/local/nii_preprocess/';
+%system(sprintf('%snii_preprocess_commit.bat',pth),'-echo');
 end
 
 
--- nii_preprocess_update.m
+++ nii_preprocess_update.m
@@ -1,6 +1,6 @@
 function nii_preprocess_update
-pth = '/usr/local/nii_preprocess/';
-system(sprintf('%snii_preprocess_update.bat',pth),'-echo');
+%pth = '/usr/local/nii_preprocess/';
+%system(sprintf('%snii_preprocess_update.bat',pth),'-echo');
 end
 
 
