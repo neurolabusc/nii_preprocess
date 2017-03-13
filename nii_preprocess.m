7741873326d9abbba5698d88b033baa42bb0580f
neurolabusc <rorden@mailbox.sc.edu>
2017-03-13 11:16:43 -0400

    comcbi-mcbi18-Mon Mar 13 11:16:43 EDT 2017

--- nii_preprocess.m
+++ nii_preprocess.m
@@ -341,7 +341,11 @@ if exist('isDki','var') && (isDki)
     warning('If you are using fsl 5.0.9, ensure you have patched version of eddy ("--repol" option) https://fsl.fmrib.ox.ac.uk/fsldownloads/patches/eddy-patch-fsl-5.0.9/centos6/');
     %2017: dti_1_eddy_cuda now auto-detects if cuda is installed
     %if isEddyCuda7Sub()
-    command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_cuda.sh'];
+    if isFullSphereSub(imgs.DKI)
+        command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_cuda.sh'];
+    else
+        command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_correct.sh'];
+    end
     %else
     %    command= [fileparts(which(mfilename)) filesep 'dti_1_eddy.sh'];
     %end
@@ -442,7 +446,11 @@ else
     %1 - eddy current correct
     %2017: dti_1_eddy_cuda now auto-detects if cuda is installed
     %if isEddyCuda7Sub()
-	command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_cuda.sh'];
+    if isFullSphereSub(imgs.DTI)
+        command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_cuda.sh'];
+    else
+        command= [fileparts(which(mfilename)) filesep 'dti_1_eddy_correct.sh'];
+    end
     %else
     %    command= [fileparts(which(mfilename)) filesep 'dti_1_eddy.sh'];
     %end
@@ -1410,7 +1418,43 @@ v = ~isempty(strfind(getenv('PATH'),'cuda'));
 %v = strcmpi(deblank(thisVer),vstr);
 %end isGpuInstalledSub()
 
-
+function [isFullSphere, meanLength] = isFullSphereSub(vNam) %does bvec sample whole sphere?
+%given FSL style bvec, determine if sampling over full or half sphere
+%useful for determining if one should run eddy (requires full sphere) or eddy_correct
+%see nii_plotBvec for visualization
+% vNam : name of bvec file
+%Output
+% isFullSphere: returns true if mean vector near zero (vectors cancel each other)
+% meanLength: length of mean vector, near zero for full sphere
+%Examples
+% isFull = nii_meanBvec('a.bvec');
+% isFull = nii_meanBvec(); %use GUI
+if ~exist('vNam','var') %no files
+ vNam = spm_select(1,'^.*\.(bvec)$','Select bvec file to view');
+end;
+%handle different extensions bvec/nii/nii.gz
+[p,n,x]=fileparts(vNam);
+if ~strcmpi(x,'.bvec')
+    fnm = fullfile(p,[n,'.bvec']);
+    if ~exist(fnm,'file') %assume img.nii.gz
+        [~,n,x] = fileparts(n);
+        fnm = fullfile(p,[n,'.bvec']);
+    end
+    if ~exist(fnm,'file')
+        error('Unable to find bvec file for %s', vNam);
+    end
+    vNam = fnm;
+end
+if ~exist(vNam, 'file'), error('Unable to find file %s',vNam); end;
+%read input
+v = textread(vNam);
+v( :, all( ~any( v ), 1 ) ) = []; %delete vectors with all zeros (e.g. B=0)
+v = mean(v,2); %mean vector
+meanLength = sqrt(sum((v) .^ 2)); %mean vector length
+%if sperical then vectors cancel out and mean length near zero
+%if half-sphere than the "center of mass" for vectors is biased
+isFullSphere = (meanLength < 0.25);
+%end isFullSphereSub()
 
 
 
